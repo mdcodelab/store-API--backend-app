d4dd28ed34231b19d80442f7b6538dde2759f533
miha11-hash <d_mihaela@msn.com>
2023-02-23 23:13:16 +0200

    populate DB - completed

--- populate.js
+++ populate.js
@@ -7,10 +7,13 @@ const jsonProducts=require("./products.json")
 const start = async () => {
     try {
        await connectDB(process.env.MONGO_URI) 
-       await Product.insertMany(jsonProducts);
+       await Product.deleteMany();
+       await Product.create(jsonProducts);  //or
+       //await Product.insertMany(jsonProducts);
        console.log("Success")
+       process.exit(0)
     } catch (error) {
-        console.log(error)
+        process.exit(1)
     }
 }
 
