254be58ecd84f8716aa1cf4190d1c602ed73992c
miha11-hash <d_mihaela@msn.com>
2023-02-24 12:27:19 +0200

    testing query params

--- controllers/products.js
+++ controllers/products.js
@@ -3,11 +3,13 @@ const Product = require("../model/product")
 const getAllProductsStatic = async (req, res) => {
     const products = await Product.find({featured: true})
     //throw new Error("testing async errors")
-    res.status(200).json({products, nmbHits: products.length})
+    res.status(200).json({Products, hbHits: products.length})
 }
 
 const getAllProducts = async (req, res) => {
-    res.status(200).json({msg: "Products route"})
+    console.log(req.query)
+    const products = await Product.find(req.query)
+    res.status(200).json({products, nbHits: products.length})
 }
 
 module.exports={getAllProductsStatic, getAllProducts}
