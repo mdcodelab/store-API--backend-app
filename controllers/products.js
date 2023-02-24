422a5b369f50a8718dbb65086b9fad929a51b00c
miha11-hash <d_mihaela@msn.com>
2023-02-24 17:01:51 +0200

    query object, filtering by features, company, name

--- controllers/products.js
+++ controllers/products.js
@@ -1,15 +1,34 @@
 const Product = require("../model/product")
 
 const getAllProductsStatic = async (req, res) => {
-    const products = await Product.find({featured: true})
+    const search='ab';
+    //const products = await Product.find({featured: true})
+    const products = await Product.find({
+        name: {$regex: search, $options: "i"}
+    })
     //throw new Error("testing async errors")
-    res.status(200).json({Products, hbHits: products.length})
+    res.status(200).json({products, hbHits: products.length})
 }
 
 const getAllProducts = async (req, res) => {
-    console.log(req.query)
-    const products = await Product.find(req.query)
+    const {featured, company, name}=req.query
+    let myObj={}
+    if (featured) {
+        myObj.featured = featured === "true" ? true : false
+    }
+
+    if(company){
+        myObj.company=company
+    }
+
+    if(name) {
+        myObj.name={$regex: name, $options: "i"}
+    }
+
+    console.log(myObj)
+    const products = await Product.find(myObj)
     res.status(200).json({products, nbHits: products.length})
 }
+
 
 module.exports={getAllProductsStatic, getAllProducts}
